25a6cde436629645ea86281cb2c0243d1da06d7a
arshedgithub <arshedahmedmail@gmail.com>
2024-06-14 23:37:41 +0530

    dao classes for v3

---
+++ 3_ServerApp/src/main/java/lk/earth/earthuniversity/dao/SupplierDao.java
@@ -0,0 +1,23 @@
+package lk.earth.earthuniversity.dao;
+
+import lk.earth.earthuniversity.entity.Supplier;
+import lk.earth.earthuniversity.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SupplierDao extends JpaRepository<Supplier,Integer> {
+
+    Optional<Supplier> findById(Integer id);
+
+    @Query("select s from Supplier s where s.id = :id")
+    Supplier findByMyId(@Param("id") Integer id);
+
+//    @Query("SELECT NEW Supplier (s.id, s.name) FROM Supplier s")
+//    List<Supplier> findAllNameId();
+
+}
+
