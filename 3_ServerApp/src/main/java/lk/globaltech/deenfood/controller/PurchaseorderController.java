8d6b3f307b61884255d1d4a380265b4dc1664329
arshedgithub <arshedahmedmail@gmail.com>
2024-08-04 21:53:31 +0530

    add v5 java functions

--- 3_ServerApp/src/main/java/lk/globaltech/deenfood/controller/PurchaseorderController.java
+++ 3_ServerApp/src/main/java/lk/globaltech/deenfood/controller/PurchaseorderController.java
@@ -1,14 +1,17 @@
 package lk.globaltech.deenfood.controller;
 
 import lk.globaltech.deenfood.dao.PurchaseOrderDao;
+import lk.globaltech.deenfood.entity.Poitem;
 import lk.globaltech.deenfood.entity.Purchaseorder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/purchaseorders")
@@ -16,120 +19,105 @@ public class PurchaseorderController {
 
     @Autowired
     private PurchaseOrderDao purchaseOrderDao;
-
     @GetMapping(produces = "application/json")
-//    @PreAuthorize("hasAuthority('ingredient-select')")
-    public List<Purchaseorder> get(@RequestParam HashMap<String, String> params) {
+//    @PreAuthorize("hasAuthority('employee-select')")
+    public List<Purchaseorder> get(@RequestParam HashMap<String,String> params) {
 
-        List<Purchaseorder> ingredients = this.purchaseOrderDao.findAll();
+        List<Purchaseorder> purorders = this.purchaseOrderDao.findAll();
 
-        if(params.isEmpty())  return ingredients;
+        if(params.isEmpty()) return purorders;
 
-        String poid = params.get("id");
-        String ponumber= params.get("ponumber");
-        String podate= params.get("date");
+        String id= params.get("id");
+        String ponumber = params.get("ponumber");
+        String date= params.get("date");
         String postatusid= params.get("postatusid");
 
-        Stream<Purchaseorder> ingredientStream = ingredients.stream();
+        Stream<Purchaseorder> purstream = purorders.stream();
 
-        if(poid!=null) ingredientStream = ingredientStream.filter(i -> i.getId() == Integer.parseInt(poid));
-        if(ponumber!=null) ingredientStream = ingredientStream.filter(i -> i.getNumber().contains(ponumber));
-        if(podate!=null) ingredientStream = ingredientStream.filter(i -> i.getDoplaced().equals(podate));
-        if(postatusid!=null) ingredientStream = ingredientStream.filter(i -> i.getPostatus().getId()==Integer.parseInt(postatusid));
+        if(ponumber!=null) purstream = purstream.filter( i -> i.getNumber().contains(ponumber));
+        if(id!=null) purstream = purstream.filter(i -> i.getId()==Integer.parseInt(id));
+        if(date!=null) purstream = purstream.filter(i -> i.getDoplaced().equals(date));
+        if (postatusid != null) purstream = purstream.filter(i -> i.getPostatus().getId()==Integer.parseInt(postatusid));
 
-        return ingredientStream.collect(Collectors.toList());
-
+        return purstream.collect(Collectors.toList());
     }
 
-//    @GetMapping(path ="/list",produces = "application/json")
-//    public List<Purchaseorder> get() {
-//
-//        List<Purchaseorder> ingredients = this.purchaseOrderDao.findAllNameId();
-//
-//        ingredients = ingredients.stream().map(
-//                ingredient -> {
-//                    Purchaseorder i = new Purchaseorder(ingredient.getId(), ingredient.getCallingname());
-//                    return  i;
-//                }
-//        ).collect(Collectors.toList());
-//
-//        return ingredients;
-//
-//    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+//    @PreAuthorize("hasAuthority('Employee-Insert')")
+    public HashMap<String,String> add(@RequestBody Purchaseorder purorder){
 
+        HashMap<String,String> response = new HashMap<>();
+        String errors="";
 
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-////    @PreAuthorize("hasAuthority('Purchaseorder-Insert')")
-//    public HashMap<String,String> add(@RequestBody Purchaseorder ingredient){
-//
-//        HashMap<String,String> response = new HashMap<>();
-//        String errors="";
-//
-//        if(purchaseOrderDao.findByName(ingredient.getName())!=null)
-//            errors = errors+"<br> Existing Name";
-//
-//        if(errors=="")
-//            purchaseOrderDao.save(ingredient);
-//        else errors = "Server Validation Errors : <br> "+errors;
-//
-//        response.put("id",String.valueOf(ingredient.getId()));
-//        response.put("url","/ingredients/"+ingredient.getId());
-//        response.put("errors",errors);
-//        return response;
-//    }
-//
-//    @PutMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-////    @PreAuthorize("hasAuthority('Purchaseorder-Update')")
-//    public HashMap<String,String> update(@RequestBody Purchaseorder ingredient){
-//
-//        HashMap<String,String> response = new HashMap<>();
-//        String errors="";
-//
-//        Purchaseorder ing = purchaseOrderDao.findByName(ingredient.getName());
-//
-//        if(ing!=null && ingredient.getId()!=ing.getId())
-//            errors = errors+"<br> Existing Name";
-//
-//
-//        if(errors=="") purchaseOrderDao.save(ingredient);
-//        else errors = "Server Validation Errors : <br> "+errors;
-//
-//        response.put("id",String.valueOf(ingredient.getId()));
-//        response.put("url","/ingredients/"+ingredient.getId());
-//        response.put("errors",errors);
-//
-//        return response;
-//    }
-//
-//
-//    @DeleteMapping("/{id}")
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public HashMap<String,String> delete(@PathVariable Integer id){
-//
-//        System.out.println(id);
-//
-//        HashMap<String,String> response = new HashMap<>();
-//        String errors="";
-//
-//        Purchaseorder ing = purchaseOrderDao.findByMyId(id);
-//
-//        if(ing==null)
-//            errors = errors+"<br> Purchaseorder Does Not Existed";
-//
-//        if(errors=="") purchaseOrderDao.delete(ing);
-//        else errors = "Server Validation Errors : <br> "+errors;
-//
-//        response.put("id",String.valueOf(id));
-//        response.put("url","/ingredients/"+id);
-//        response.put("errors",errors);
-//
-//        return response;
-//    }
+        Purchaseorder grn1 = purchaseOrderDao.findByMyId(purorder.getId());
+        if(grn1!=null && purorder.getId()!=grn1.getId())
+            errors = errors+"<br>Purchase Order Not Found";
 
+        for (Poitem poitems : purorder.getPoitems()) {
+            poitems.setPurchaseorder(purorder);
+        }
+
+        if(errors=="")
+            purchaseOrderDao.save(purorder);
+        else errors = "Server Validation Errors : <br> "+errors;
+
+        response.put("id",String.valueOf(purorder.getId()));
+        response.put("url","/purchaseorders/"+purorder.getId());
+        response.put("errors",errors);
+
+        return response;
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.CREATED)
+//    @PreAuthorize("hasAuthority('Employee-Update')")
+    public HashMap<String,String> update(@RequestBody Purchaseorder purorder) {
+
+        HashMap<String, String> response = new HashMap<>();
+        String errors = "";
+
+        Purchaseorder itm = purchaseOrderDao.findByMyId(purorder.getId());
+        if (itm != null && purorder.getId() != itm.getId())
+            errors = errors + "<br> Existing Number";
+
+        for (Poitem poItem : purorder.getPoitems()) {
+            poItem.setPurchaseorder(purorder);
+        }
+
+        if (errors == "") purchaseOrderDao.save(purorder);
+        else errors = "Server Validation Errors : <br> " + errors;
+
+        response.put("id", String.valueOf(purorder.getId()));
+        response.put("url", "/purchaseorders/" + purorder.getId());
+        response.put("errors", errors);
+
+        return response;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public HashMap<String,String> delete(@PathVariable Integer id){
+
+        System.out.println(id);
+
+        HashMap<String,String> response = new HashMap<>();
+        String errors="";
+
+        Purchaseorder itm = purchaseOrderDao.findByMyId(id);
+
+        if(itm==null)
+            errors = errors+"<br> Employee Does Not Existed";
+
+        if(errors=="") purchaseOrderDao.delete(itm);
+        else errors = "Server Validation Errors : <br> "+errors;
+
+        response.put("id",String.valueOf(id));
+        response.put("url","/purchaseorders/"+id);
+        response.put("errors",errors);
+
+        return response;
+    }
 }
-
-
 
 
--- 3_ServerApp/src/main/java/lk/globaltech/deenfood/dao/PoitemDao.java
+++
@@ -1,9 +0,0 @@
-package lk.globaltech.deenfood.dao;
-
-import lk.globaltech.deenfood.entity.Poitem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PoitemDao extends JpaRepository<Poitem,Integer> {
-
-}
-
