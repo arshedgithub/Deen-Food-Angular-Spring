fae4ffc91dece3e52401c4e4101a5d412ca86ed2
arshedgithub <arshedahmedmail@gmail.com>
2024-08-18 14:18:34 +0530

    add UCSC code modification

--- 2_ClientApp/src/app/entity/product.ts
+++ 2_ClientApp/src/app/entity/product.ts
@@ -9,6 +9,7 @@ export class Product {
   public name !: string;
   public description !: string;
   public quantity !: number;
+  public rop: number;
   public price !: number;
   public photo !: string;
   public dointroduced !: string;
@@ -16,13 +17,14 @@ export class Product {
   public productIngredients !: Array<Productingredient>;
   public employee !: Employee;
 
-  constructor(id: number, productnumber: string, name: string, description: string, quantity: number, price: number, photo: string, dointroduced: string, productstatus: Productstatus, productIngredients: Array<Productingredient>, employee: Employee) {
+  constructor(id: number, productnumber: string, name: string, description: string, quantity: number, price: number, photo: string, dointroduced: string, productstatus: Productstatus, productIngredients: Array<Productingredient>, employee: Employee, rop: number) {
     this.id = id;
     this.productnumber = productnumber;
     this.name = name;
     this.description = description;
     this.quantity = quantity;
     this.price = price;
+    this.rop = rop;
     this.photo = photo;
     this.dointroduced = dointroduced;
     this.productstatus = productstatus;
